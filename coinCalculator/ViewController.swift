9457165a87d8475a619a5276a158b821aadd9e9d
George Vytka <george.vytka@yahoo.com>
2019-03-22 16:34:49 -0400

    Initial Commit

---
+++ coinCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coinCalculator
+//
+//  Created by George Vytka on 3/22/19.
+//  Copyright © 2019 George Vytka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
